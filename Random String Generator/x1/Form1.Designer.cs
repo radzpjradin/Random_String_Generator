0fd507d63c43ba69ba823eb00b807a74f3e89e36
PJ Radz <radzpjradin@gmail.com>
2017-09-27 17:50:41 +0800

    Add files via upload

---
+++ Random String Generator/x1/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace x1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(242, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(323, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(52, 82);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(54, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "length:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 146);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(94, 17);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Alpha-Numeric";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(129, 146);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(64, 17);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Numeric";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(242, 146);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(67, 17);
+            this.radioButton3.TabIndex = 6;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Alphabet";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 261);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.MaximumSize = new System.Drawing.Size(363, 300);
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "Form1";
+            this.Text = "Random Password Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ Random String Generator/x1/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace x1
+{
+    public partial class Form1 : Form
+    {
+        bool nonNumberEntered = true;
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.ReadOnly = true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+   
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringGen.stringc str = new StringGen.stringc();
+            if (textBox2.Text.Length != 0)
+            {
+                int a = Int32.Parse(textBox2.Text);
+                if (radioButton1.Checked)
+                    textBox1.Text = str.AlphaNumeric(a);
+                else if (radioButton2.Checked)
+                    textBox1.Text = str.Numeric(a);
+                else if (radioButton3.Checked)
+                    textBox1.Text = str.Alphabet(a);
+                else
+                    textBox1.Text = "Please Choose a type of Password";
+            }
+            else
+                textBox1.Text = "Please Type the Length Value";
+
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            nonNumberEntered = false;
+
+            if (e.KeyCode < Keys.D0 || e.KeyCode > Keys.D9)
+            {
+                if (e.KeyCode < Keys.NumPad0 || e.KeyCode > Keys.NumPad9)
+                {
+                    if (e.KeyCode != Keys.Back)
+                    {
+                        nonNumberEntered = true;
+                    }
+                }
+            }
+            if (Control.ModifierKeys == Keys.Shift)
+            {
+                nonNumberEntered = true;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (nonNumberEntered == true)
+            {
+                e.Handled = true;
+            }
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+namespace StringGen
+{
+    public class stringc
+    {
+        public string AlphaNumeric(int len)
+        {
+            Random r = new Random();
+            int l = 0;
+            string a = null;
+            while(l < len)
+            {
+                int asc = r.Next() % 100;
+                if (asc > -1 && asc < 26)
+                {
+                    int decision = r.Next() % 3;
+                    if (decision == 0)
+                    {
+                        int base0 = 65;
+                        base0 += asc;
+                        char i = (char)base0;
+                        a += i.ToString();
+                        l++;
+                    }
+                    else if (decision == 1)
+                    {
+                        int base0 = 97;
+                        base0 += asc;
+                        char i = (char)base0;
+                        a += i.ToString();
+                        l++;
+                    }
+                    else
+                    {
+                        int x = r.Next() % 10;
+                        a += x.ToString();
+                        l++;
+                    }
+                }
+                else
+                    asc = r.Next() % 100;
+            }
+            return a;
+        }
+        public string Numeric(int len)
+        {
+            Random r = new Random();
+            int l = 0;
+            string a = null;
+            while(l < len)
+            {
+                int x = r.Next() % 10;
+                a += x.ToString();
+                l++;
+            }
+            return a;
+        }
+        public string AlphaCap(int len)
+        {
+
+            string a = null;
+            Random r = new Random();
+            int l = 0;
+            while(l < len)
+            {
+                int asc = r.Next() % 100;
+                if (asc > -1 && asc < 26)
+                {
+                    int base0 = 65;
+                    base0 += asc;
+                    char i = (char)base0;
+                    a += i.ToString();
+                    l++;
+                }
+                else
+                    asc = r.Next() % 100;
+            }
+            return a;
+        }
+        public string AlphaSmall(int len)
+        {
+
+            string a = null;
+            Random r = new Random();
+            int l = 0;
+            while (l < len)
+            {
+                int asc = r.Next() % 100;
+                if (asc > -1 && asc < 26)
+                {
+                    int base0 = 97;
+                    base0 += asc;
+                    char i = (char)base0;
+                    a += i.ToString();
+                    l++;
+                }
+                else
+                    asc = r.Next() % 100;
+            }
+            return a;
+        }
+        public string Alphabet(int len)
+        {
+            string a = null;
+            Random r = new Random();
+            int l = 0;
+            while(l < len)
+            {
+                int asc = r.Next() % 100;
+                if (asc > -1 && asc < 26)
+                {
+                    int decision = r.Next() % 2;
+                    if (decision == 0)
+                    {
+                        int base0 = 97;
+                        base0 += asc;
+                        char i = (char)base0;
+                        a += i.ToString();
+                        l++;
+                    }
+                    else
+                    {
+                        int base0 = 65;
+                        base0 += asc;
+                        char i = (char)base0;
+                        a += i.ToString();
+                        l++;
+                    }
+                }
+                else
+                    asc = r.Next() % 100;
+            }
+            return a;
+        }
+    }
+}
